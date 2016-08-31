4678cfcf30fe9ea798cec25179a6bba17f51ca24
LuK1337 <priv.luk@gmail.com>
2016-08-31 10:42:00 -0400

    v4xx: Build telephony-exto
    Change-Id: I6a3f2dfd27979cb7cdad96a0568602fa7cd9c0d1
    Signed-off-by: Adam Myczkowski <mycaxd511@gmail.com>

--- v4xx.mk
+++ v4xx.mk
@@ -212,6 +212,10 @@ PRODUCT_PACKAGES += \
     init.zetaw.usb.sh \
     ueventd.e7.rc
 
+# Telephony-ext
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8226.conf:system/etc/thermal-engine-8226.conf
