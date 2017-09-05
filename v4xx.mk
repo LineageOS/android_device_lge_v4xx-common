e34c15f8132e7b13f184f5f99558d24ebc19645b
LuK1337 <priv.luk@gmail.com>
2017-09-05 19:53:26 +0200

    v4xx-common: Build our MultiHAL sensors HAL
    Change-Id: Icce5b5f487321e79c7657ac66eefd571065ee010

--- v4xx.mk
+++ v4xx.mk
@@ -254,8 +254,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
 
 # Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf
+
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    sensors.msm8226
 
 # USB HIDL interfaces
 PRODUCT_PACKAGES += \
