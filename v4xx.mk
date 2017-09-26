697a2c4f05d9bee3687cc4896e9c349bf7292240
Adam Myczkowski <mycaxd511@gmail.com>
2017-09-26 20:11:00 +0100

    v4xx-common: Add seccomp policy
    Change-Id: I96b3c2182fb45ac7e2a2ee36bdd5f8ae72c0ca2a
    Signed-off-by: Adam Myczkowski <mycaxd511@gmail.com>

--- v4xx.mk
+++ v4xx.mk
@@ -248,6 +248,11 @@ PRODUCT_PACKAGES += \
     init.zetaw.usb.sh \
     ueventd.v4xx.rc
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/configs/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
